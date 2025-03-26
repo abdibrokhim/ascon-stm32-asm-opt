bc8af5f19de5532217a843fe510c346da2e838bf
abdibrokhim <abdibrokhim@gmail.com>
2025-03-26 23:32:50 +0500

    it took 04:33 secs to complete

--- Debug/Core/Src/Gost_Old/gost_old.d
+++ Debug/Core/Src/Gost_Old/gost_old.d
@@ -1,3 +1,1 @@
-Core/Src/Gost_Old/gost_old.o: ../Core/Src/Gost_Old/gost_old.cpp \
- ../Core/Inc/gost28147_89/gost_old.h
-../Core/Inc/gost28147_89/gost_old.h:
+Core/Src/Gost_Old/gost_old.o: ../Core/Src/Gost_Old/gost_old.cpp
